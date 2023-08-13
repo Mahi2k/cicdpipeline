15177543b97bdf05312a0e913d782199673d28fe
Mahendra Tandon <mahendratandon.2k@gmail.com>
2023-08-13 12:50:13 +0530

    Added file write code

--- latestcommit.py
+++ latestcommit.py
@@ -15,14 +15,33 @@ headers = {
     "User-Agent": "Python GitHub API Client"
 }
 
+old_commitId = ""
+
+def writeCommitId(id):
+    f_object = open('latest-commit.txt', 'w')
+    f_object.write(id)
+    f_object.close()
+
+def readCommitId():
+    f_object = open('latest-commit.txt', 'r')
+    commitId = f_object.read()
+    f_object.close()
+    return commitId
+
 # Make a GET request to the API
 response = requests.get(url, headers=headers)
-
 # Check if the request was successful
 if response.status_code == 200:
     commits = response.json()  # Parse JSON from the response
-    for commit in commits:
-        commit_id = commit['sha']
-        print(commit_id)
+    # for commit in commits:
+    firstCommit = commits[0]
+    latest_commit_id = firstCommit["sha"]
+    print(latest_commit_id)
+    
+    previousCommitId = readCommitId()
+    print("Prev",previousCommitId)
+    print("First", firstCommit)
+    if(previousCommitId != latest_commit_id):
+       writeCommitId(latest_commit_id) 
 else:
     print("Request failed with status code:", response.status_code)
